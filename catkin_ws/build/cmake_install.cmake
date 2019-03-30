8897326303ab0afbb76517d7515fb9cac9894cf9
eogiesoba <eogiesoba@gmail.com>
2019-03-30 21:38:10 +0000

    Add new pgm_map_creator files

--- catkin_ws/build/cmake_install.cmake
+++ catkin_ws/build/cmake_install.cmake
@@ -118,8 +118,6 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/workspace/RSE_Where_Am_I/catkin_ws/build/gtest/cmake_install.cmake")
   include("/home/workspace/RSE_Where_Am_I/catkin_ws/build/my_robot/cmake_install.cmake")
-  include("/home/workspace/RSE_Where_Am_I/catkin_ws/build/simple_arm/cmake_install.cmake")
-  include("/home/workspace/RSE_Where_Am_I/catkin_ws/build/ball_chaser/cmake_install.cmake")
   include("/home/workspace/RSE_Where_Am_I/catkin_ws/build/pgm_map_creator/cmake_install.cmake")
 
 endif()
